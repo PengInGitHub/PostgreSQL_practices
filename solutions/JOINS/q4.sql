2bcb6b3cc3252363c1edf87f9ed90fa4c9a5ff09
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 19:50:00 +0100

    finish q4, one more condition than q3

---
+++ solutions/JOINS/q4.sql
@@ -0,0 +1,3 @@
+//Make a list of customers who appointed a salesman for their jobs who gets a commission is above 12%
+//Write a SQL statement to find the list of customers who appointed a salesman for their jobs who gets a commission from the company is more than 12%.
+SELECT a.cust_name AS "customer name", a.city,b.name,b.commission FROM customer a,salesman b WHERE a.salesman_id=b.salesman_id AND b.commission>0.12;
