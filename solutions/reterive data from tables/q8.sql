9943135787e8ae062b42163dce49d5ee69dfe58c
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 16:29:09 +0100

    add q8, select without any repeats

---
+++ solutions/reterive data from tables/q8.sql
@@ -0,0 +1,2 @@
+//Write a query which will retrieve the values of salesman id of all salesmen getting orders from the customers in orders table without any repeats.
+SELECT DISTINCT salesman_id FROM orders;
