a3335031e3d60190f4a5c72adf37cb73d92319ed
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 19:42:05 +0100

    finish q2, assign variable names to tables (a,b)

--- solutions/JOINS/q2.sql
+++ solutions/JOINS/q2.sql
@@ -1,2 +1,2 @@
 //Write a SQL statement to make a list with order no, purchase amount, customer name and their cities for those orders which order amount between 500 and 2000.
-SELECT orders.ord_no,orders.purch_amt,customer.cust_name,customer.city FROM customer, orders WHERE orders.customer_id=customer.customer_id AND purch_amt BETWEEN 500 AND 2000;
+SELECT b.ord_no,b.purch_amt,a.cust_name,a.city FROM customer a, orders b WHERE b.customer_id=a.customer_id AND purch_amt BETWEEN 500 AND 2000;
