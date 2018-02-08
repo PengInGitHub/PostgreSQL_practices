8f93ef5e5cf1f258b63f4a3b611f05d0bce73394
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 16:37:33 +0100

    add q10, select aginst specif conditions --- where

---
+++ solutions/reterive data from tables/q10.sql
@@ -0,0 +1,3 @@
+// Select all columns against a specified condition
+//Write a SQL statement to display all the information for those customers with a grade of 200.
+SELECT * FROM customer WHERE grade=200;
