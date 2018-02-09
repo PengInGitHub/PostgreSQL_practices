78390d56cb99bc74c2f3428f778420991b98cfc3
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 13:53:11 +0100

    init q27

---
+++ solutions/JOINS/q27.sql
@@ -0,0 +1,3 @@
+//Display the first name and last name of each employee, along with the name and sacntion amount for their department
+
+//Write a query in SQL to display the first name and last name of each employee, along with the name and sacntion amount for their department.
