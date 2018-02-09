549997aa01eae0e686f8792ff68f7b21c7c426f2
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 10:42:41 +0100

    add q17, add defination of CROSS JOIN, which returns a result set which is the rows of table left produces the row of table right

--- solutions/JOINS/q17.sql
+++ solutions/JOINS/q17.sql
@@ -1,3 +1,9 @@
 //Prepare a cartesian product between salesman and customer i.e. each salesman will appear for all customer and vice versa
 
 //Write a SQL statement to make a cartesian product between salesman and customer i.e. each salesman will appear for all customer and vice versa. 
+
+
+//Define Cross Join
+//The SQL CROSS JOIN produces a result set which is the number of rows in the first table multiplied by the number of rows in the second table if no WHERE clause is used along with CROSS JOIN.
+//This kind of result is called as Cartesian Product.
+
