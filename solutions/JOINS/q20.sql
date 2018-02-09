cc546c9192be863aafd6d462dd7864fe0a9754ef
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 10:50:18 +0100

    init q20, CROSS JOIN with conditions, <>

---
+++ solutions/JOINS/q20.sql
@@ -0,0 +1,4 @@
+//Prepare a cartesian product between salesman and customer i.e. each salesman will appear for all customer and vice versa for those salesmen 
+//who must belong a city which is not the same as his customer and the customers should have an own grade
+
+
