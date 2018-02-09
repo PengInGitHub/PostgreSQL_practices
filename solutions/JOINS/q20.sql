c4adf1f6b4fde2afd62536d0bb05161cffa3e499
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 10:50:44 +0100

    finish q20, CROSS JOIN with conditions, <>

--- solutions/JOINS/q20.sql
+++ solutions/JOINS/q20.sql
@@ -1,4 +1,4 @@
 //Prepare a cartesian product between salesman and customer i.e. each salesman will appear for all customer and vice versa for those salesmen 
 //who must belong a city which is not the same as his customer and the customers should have an own grade
 
-
+SELECT * FROM salesman CROSS JOIN customer WHERE salesman.city <> customer.city AND customer.grade IS NOT NULL AND salesman.city IS NOT NULL;
