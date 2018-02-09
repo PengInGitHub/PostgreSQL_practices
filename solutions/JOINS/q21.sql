0b6fb4d684a2502262870e02f5974de16b674a8f
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 11:09:11 +0100

    finish q21,  INNER JOIN

--- solutions/JOINS/q21.sql
+++ solutions/JOINS/q21.sql
@@ -3,4 +3,4 @@
 //deinfe INNER JOIN
 //The INNER JOIN keyword selects records that have matching values in both tables.
 
-
+SELECT * FROM item_mast INNER JOIN company_mast ON item_mast.pro_com=company_mast.com_id;
