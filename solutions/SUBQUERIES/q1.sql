ef6ea84ae02cba9ace7a3d47dd037af4bb49c994
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 15:19:02 +0100

    add q1, WHERE salesman_id=(SELECT salesman_id FROM salesman WHERE name='Paul Adam')

--- solutions/SUBQUERIES/q1.sql
+++ solutions/SUBQUERIES/q1.sql
@@ -1,1 +1,6 @@
+//Display all the orders from the orders table issued by the salesman 'Paul Adam'.
 
+
+//1. Write a query to display all the orders from the orders table issued by the salesman 'Paul Adam'.
+
+SELECT * FROM orders WHERE salesman_id=(SELECT salesman_id FROM salesman WHERE name='Paul Adam');
