8d583d95d8d4f32a178fbcacee9e20290b7dfd16
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 12:37:48 +0100

    add q22, INNER JOIN

--- solutions/JOINS/q22.sql
+++ solutions/JOINS/q22.sql
@@ -1,1 +1,7 @@
+//Display the item name, price, and company name of all the products
+//Write a SQL query to display the item name, price, and company name of all the products.
 
+
+//INNER JOIN
+
+SELECT A.PRO_NAME,A.PRO_PRICE,B.COM_NAME FROM item_mast A INNER JOIN company_mast B ON A.PRO_COM=B.COM_ID;
