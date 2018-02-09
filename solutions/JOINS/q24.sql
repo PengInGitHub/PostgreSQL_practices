a41bf4d34a4092285ba9e87b2cb24d86b74bda23
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 12:53:47 +0100

    init q24Y

---
+++ solutions/JOINS/q24.sql
@@ -0,0 +1,5 @@
+//Display the names of the company whose products have an average price larger than or equal to Rs.350
+
+//Write a SQL query to display the average price of items of each company, showing the name of the company.
+
+
