06e0397d83661188639eb098cfa97a1973eb698a
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 12:56:17 +0100

    add q24, INNER JOIN, AVG GROUP BY HAVING condition

--- solutions/JOINS/q24.sql
+++ solutions/JOINS/q24.sql
@@ -2,4 +2,4 @@
 
 //Write a SQL query to display the average price of items of each company, showing the name of the company.
 
-
+SELECT AVG(A.PRO_PRICE),B.COM_NAME FROM item_mast A JOIN company_mast B ON A.PRO_COM=B.COM_ID GROUP BY B.com_name HAVING AVG(A.PRO_PRICE)>300;
