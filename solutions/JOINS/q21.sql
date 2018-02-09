d05d1452ca172049cbd00f620996e89aca030149
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 11:04:53 +0100

    add q21, add defination of INNER JOIN

--- solutions/JOINS/q21.sql
+++ solutions/JOINS/q21.sql
@@ -1,1 +1,6 @@
 //Display all the data from the item_mast, including all the data for each item's producer company
+
+//deinfe INNER JOIN
+//The INNER JOIN keyword selects records that have matching values in both tables.
+
+
