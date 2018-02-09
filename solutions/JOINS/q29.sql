5a5755c389c1d4e9dd423bfcd4d18961a5bf76d4
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 14:15:08 +0100

    finish q29,INNER JOIN ON, GROUP BY, HAVING, COUNT(*)>2

--- solutions/JOINS/q29.sql
+++ solutions/JOINS/q29.sql
@@ -3,4 +3,4 @@
 //Write a query in SQL to find the names of departments where more than two employees are working.
 
 
-
+SELECT emp_department.DPT_NAME FROM emp_department INNER JOIN emp_details ON emp_details.EMP_DEPT=emp_department.DPT_CODE GROUP BY emp_department.DPT_NAME HAVING COUNT(*)>2;
