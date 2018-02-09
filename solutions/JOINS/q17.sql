301abbd5d7b54e9f44898cf9ffbc6a856063f5d7
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-09 10:32:05 +0100

    init q17

---
+++ solutions/JOINS/q17.sql
@@ -0,0 +1,3 @@
+//Prepare a cartesian product between salesman and customer i.e. each salesman will appear for all customer and vice versa
+
+//Write a SQL statement to make a cartesian product between salesman and customer i.e. each salesman will appear for all customer and vice versa. 
